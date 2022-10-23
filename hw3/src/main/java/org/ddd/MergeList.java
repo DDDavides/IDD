232521fcdc3643e43710b0fccd6b512370776c93
Davide Molitierno <>
2022-10-23 15:20:47 +0200

    Modifiche alla classe mergeList

--- hw3/src/main/java/org/ddd/MergeList.java
+++ hw3/src/main/java/org/ddd/MergeList.java
@@ -2,52 +2,71 @@ package org.ddd;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TermQuery;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.*;
 
 import java.util.*;
 
 public class MergeList {
-
-    private static final int  K = 5;
-
-    public List<String> topKOverlapMerge(int k, IndexSearcher searcher, List<String> columnElements) throws Exception {
-        //TODO: implementare il metodo
-        if(k <= 0){
+    /**
+     * Metodo che ritorna i primi top topk
+     * degli elementi all'interno di un searcher
+     * in un indici con gli elementi della query passati come parametro
+     * @param topk
+     * @param searcher
+     * @param columnElements
+     * @return
+     * @throws Exception
+     */
+    public static List<String> topKOverlapMerge(int topk, IndexSearcher searcher, List<String> columnElements) throws Exception {
+        //se il numero di elementi richiesti topk è minore
+        //o uguale di 0 lancia un eccezione
+        if(topk <= 0){
             throw new Exception();
         }
-
-        HashMap<String, Integer> column2frequency = new LinkedHashMap<>();
+        //riempi la mappa colonna termini contenuti in base all'overlap
+        //tra la query e la colonna
+        HashMap<String, Integer> column2frequency = new HashMap<>();
         for(String element: columnElements){
-            TopDocs docs = searcher.search(new TermQuery(new Term(element)), 5);
+            //per ogni termine della query cerca le colonne che fanno hit
+            TopDocs docs = searcher.search(new BooleanQuery.Builder()
+                    .add(new PhraseQuery("colonna", element), BooleanClause.Occur.MUST).
+                    build(), -1);
+            //popola la mappa con le colonne ritornate
             for(int i = 0; i < docs.scoreDocs.length; i++){
                 ScoreDoc scoreDoc = docs.scoreDocs[i];
                 Document doc = searcher.doc(scoreDoc.doc);
+                //se la colonna è già presente nella mappa
                 if (column2frequency.containsKey(doc.toString())){
                     column2frequency.put(doc.toString(), column2frequency.get(doc) + 1);
                 }else {
                     column2frequency.put(doc.toString(), 1);
                 }
-
             }
         }
+        //ordina la mappa per i valori che fanno più overlap
         column2frequency = sortByValue(column2frequency);
         List<String> topKoverlapElements = new LinkedList<>();
-        for(int i = 0; i < k; i++){
-            topKoverlapElements.add(column2frequency.get(i));
+        //ritorna solo le prime topk colonne
+        String[] columns = (String[]) column2frequency.keySet().toArray();
+        for(int i = 0; i < topk; i++){
+            topKoverlapElements.add(columns[i]);
         }
         return topKoverlapElements;
     }
 
-    public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm)
+    /**
+     * Metodo che ordina in base ai campi value
+     * una mappa passata come parametro
+     * @param map
+     * @return
+     */
+    public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> map)
     {
-        // Create a list from elements of HashMap
+        // Crea una lista di elementi con i valori della mappa
         List<Map.Entry<String, Integer> > list =
-                new LinkedList<>(hm.entrySet());
+                new LinkedList<>(map.entrySet());
 
-        // Sort the list
+        // Ordina la lista
         Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() {
             public int compare(Map.Entry<String, Integer> o1,
                                Map.Entry<String, Integer> o2)
@@ -56,7 +75,7 @@ public class MergeList {
             }
         });
 
-        // put data from sorted list to hashmap
+        // Metti i dati ordinati della lista in una hashmap
         HashMap<String, Integer> temp = new LinkedHashMap<>();
         for (Map.Entry<String, Integer> aa : list) {
             temp.put(aa.getKey(), aa.getValue());
