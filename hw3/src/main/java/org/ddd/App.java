5042730c15832433328fa36cce3a62190cabe6b4
Davide Molitierno <>
2022-10-25 20:14:56 +0200

    Miglioramenti nella classe di MergeList

--- hw3/src/main/java/org/ddd/App.java
+++ hw3/src/main/java/org/ddd/App.java
@@ -1,13 +1,39 @@
 package org.ddd;
 
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static String indexPath;
+
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get(indexPath);
+        IndexSearcher searcher;
+        try (Directory directory = FSDirectory.open(path)) {
+            try (IndexReader reader = DirectoryReader.open(directory)) {
+                searcher = new IndexSearcher(reader);
+                //per ogni termine della query cerca tutte le colonne che fanno hit
+                MergeList ml = new MergeList(searcher);
+                ml.topKOverlapMerge(5, new ArrayList<>());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            } finally {
+                directory.close();
+            }
+        }
     }
 }
--- hw3/src/main/java/org/ddd/MergeList.java
+++ hw3/src/main/java/org/ddd/MergeList.java
@@ -4,15 +4,20 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.*;
 
+import java.io.IOException;
 import java.util.*;
 
 public class MergeList {
+
+    private static IndexSearcher searcher;
+    public MergeList(IndexSearcher searcher){
+        searcher = searcher;
+    }
     /**
      * Metodo che ritorna i primi top topk
      * degli elementi all'interno di un searcher
      * in un indici con gli elementi della query passati come parametro
      * @param topk
-     * @param searcher
      * @param columnElements
      * @return
      * @throws Exception
@@ -20,7 +25,7 @@ public class MergeList {
     //TODO: sostituire searcher con lista di risultati e
     // creare apposita classe searcher che effettua la
     // ricerca della query e gli passa tale lista di risultati
-    public static List<String> topKOverlapMerge(int topk, IndexSearcher searcher, List<String> columnElements) throws Exception {
+    public List<String> topKOverlapMerge(int topk, List<String> columnElements) throws Exception {
         //se il numero di elementi richiesti topk è minore
         //o uguale di 0 lancia un eccezione
         if(topk <= 0){
@@ -30,17 +35,9 @@ public class MergeList {
         //tra la query e la colonna
         HashMap<String, Integer> column2frequency = new HashMap<>();
         for(String element: columnElements){
-            //per ogni termine della query cerca tutt le colonne che fanno hit
-            TotalHitCountCollector collector = new TotalHitCountCollector();
-            BooleanQuery booleanQuery = new BooleanQuery.Builder()
-                    .add(new PhraseQuery("colonna", element), BooleanClause.Occur.MUST).
-                    build();
-            searcher.search(booleanQuery, collector);
-            TopDocs docs = searcher.search(booleanQuery, collector.getTotalHits());
+            List<Document> documents = search(element);
             //popola la mappa con le colonne ritornate
-            for(int i = 0; i < docs.scoreDocs.length; i++){
-                ScoreDoc scoreDoc = docs.scoreDocs[i];
-                Document doc = searcher.doc(scoreDoc.doc);
+            for(Document doc : documents){
                 //se la colonna è già presente nella mappa
                 if (column2frequency.containsKey(doc.toString())){
                     column2frequency.put(doc.toString(), column2frequency.get(doc) + 1);
@@ -58,6 +55,23 @@ public class MergeList {
             topKoverlapElements.add(columns[i]);
         }
         return topKoverlapElements;
+    }
+
+    private static List<Document> search(String element) throws IOException {
+        List<Document> documents = new ArrayList<>();
+        TotalHitCountCollector collector = new TotalHitCountCollector();
+        BooleanQuery booleanQuery = new BooleanQuery.Builder()
+                .add(new PhraseQuery("colonna", element), BooleanClause.Occur.MUST).
+                build();
+        searcher.search(booleanQuery, collector);
+        TopDocs docs = searcher.search(booleanQuery, collector.getTotalHits());
+        //popola la mappa con le colonne ritornate
+        for(int i = 0; i < docs.scoreDocs.length; i++) {
+            ScoreDoc scoreDoc = docs.scoreDocs[i];
+            Document doc = searcher.doc(scoreDoc.doc);
+            documents.add(doc);
+        }
+        return documents;
     }
 
     /**
