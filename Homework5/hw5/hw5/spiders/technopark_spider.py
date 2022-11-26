c64335cc50a73a5f75eff6bd38a24c6399d77eb7
Davide Galletti <d.galletti99@gmail.com>
2022-11-26 20:01:58 +0100

    Da sistemare - Crawler non funzionante per pagine allocate dinamicamente

--- Homework5/hw5/hw5/spiders/technopark_spider.py
+++ Homework5/hw5/hw5/spiders/technopark_spider.py
@@ -1,22 +1,51 @@
 import scrapy
+from scrapy_playwright.page import PageMethod
 
-class CbinsightSpider(scrapy.Spider):
+class TechnoparkSpider(scrapy.Spider):
     name = 'technopark'
     allowed_domains = ['www.technopark.org']
     start_urls = [
         'https://www.technopark.org/company-a-z-listing'
     ]
+    base_url = 'https://www.technopark.org'
+
+    def start_requests(self):
+        meta = dict(
+            playwright = True,
+            playwright_page_methods = [
+                PageMethod('evaluate', "window.scrollBy(0, document.body.scrollHeight)"),
+                PageMethod("wait_for_selector", "div.cmpny-detail"),
+            ],
+            playwright_include_page = True,
+            errback=self.errback
+        )
+        yield scrapy.Request(self.start_urls[0], meta=meta)
+
 
     #Rules
-    def parse(self, response):
-        all_companies = response.xpath("//div[contains(@class, 'cmpny-detail')]//a[@href][1]/@href")
-        print(all_companies)
+    async def parse(self, response):
+        page = response.meta['playwright_page']
+        nEntry = 4
+        maxEntry = 1000
+        for i in range(1, maxEntry // nEntry):
+            x = i * nEntry
+            await page.evaluate("window.scrollBy(0, document.body.scrollHeight)")
+            await page.wait_for_selector("div.cmpny-detail:nth-child({})".format(x))
+        
+        html = await page.content()
+        await page.close()
+        s = scrapy.Selector(text=html)
+
+        all_companies = s.xpath("//div[contains(@class,'cmpny-detail')]//a[@href][1]/@href")
         for company in all_companies:
-            print(company.get())
-            yield scrapy.Request(company.get(), callback=self.parse_company)
+            print(company)
+            yield scrapy.Request(self.base_url + company.get(), callback=self.parse_company)
         
     def parse_company(self, response):
         if response.status != 200:
-            return
+            yield
         # print(response.url)
-        return ""
+
+    async def errback(self, failure):
+        page = failure.request.meta["playwright_page"]
+        await page.close()
