44d10babfc77853eccec200af2505d18b9686d73
Davide Molitierno <>
2022-11-27 23:54:38 +0100

    Integrate ulteriori informazioni per cbinsight

--- Homework5/hw5/hw5/spiders/cbinsight_spider.py
+++ Homework5/hw5/hw5/spiders/cbinsight_spider.py
@@ -14,15 +14,12 @@ class CbinsightSpider(scrapy.Spider):
 
     def parse(self, response):
         bs = BeautifulSoup(response.text, 'lxml')
-        self.extract_data_from_table(bs)
-
-    def extract_data_from_table(self, bs):
+        table = []
         trow = bs.find_all('tr')
         print(len(trow))
-        table = []
         for i in range(1, len(trow)):
             children_row = trow[i].findChildren('td', recursive=False)
-            table.append({
+            row = {
                 "Name": children_row[0].getText(),
                 "Valuation": children_row[1].getText(),
                 "Date Joined": children_row[2].getText(),
@@ -30,34 +27,37 @@ class CbinsightSpider(scrapy.Spider):
                 "City": children_row[4].getText(),
                 "Industry": children_row[5].getText(),
                 "Investors": children_row[6].getText()
-            })
-            # if j == 0:
-            #     yield scrapy.Request(children_row[j].find('a')['href'], callback=self.parse_company)
+            }
+            print(children_row[0].find('a')['href'])
+            yield scrapy.Request(children_row[0].find('a')['href'], callback=self.parse_company, cb_kwargs=dict(row=row, table=table))
         csv_file = './dataset/cbinsight.csv'
-        csv_columns = ['Name','Valuation',"Date Joined","Country","City","Industry", "Investors"]
-        with open(csv_file, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-            writer.writeheader()
-            for data in table:
-                writer.writerow(data)
+        csv_columns = ['Name','Valuation',"Date Joined","Country","City","Industry", "Investors", "founded", "stage", "totalRaised"]
+        dict_to_csv(table, csv_file, csv_columns)
         
-    def parse_company(self, response):
+    def parse_company(self, response, row, table):
         if response.status != requests.codes.ok:
             return
         bs = BeautifulSoup(response.text, 'lxml')
         title = bs.find('h1').getText()
         h = bs.find_all('h2', {'class': 'text-sm text-black'})
-        founded, stage, totalRaised = None, None, None
         for i in range(0, len(h)-1):
             if i == 0:
-                founded = h[i].next_sibling.getText()
+                row['founded'] = h[i].next_sibling.getText()
             elif i == 1: 
-                stage = h[i].next_sibling.getText()
+                row['stage'] = h[i].next_sibling.getText()
             elif i == 2:
-                totalRaised = h[i].next_sibling.getText()
+                row['totalRaised'] = h[i].next_sibling.getText()
         if title == None:
             print(response.url)
+        table.append(row)
 
+
+def dict_to_csv(table, csv_file, csv_columns):
+        with open(csv_file, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            writer.writeheader()
+            for data in table:
+                writer.writerow(data)
 
 def myselector(tag):
     return tag.name == 'td' and tag.findChildren('a')
