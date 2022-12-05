333c4f3cfa6ac30c221f40a944d2572845526687
Davide Molitierno <>
2022-12-05 15:42:39 +0100

    Reso parametrico il numero di elementi preso

--- Homework5/hw5/hw5/run_script.py
+++ Homework5/hw5/hw5/run_script.py
@@ -6,7 +6,7 @@ process = CrawlerProcess(get_project_settings())
 
 process.crawl('cbinsight')
 process.crawl('companiesmarketcap')
-# process.crawl('financial')
-process.crawl('technopark')
+process.crawl('financial')
+#process.crawl('technopark')
 process.crawl('teamblind')
 process.start() # the script will block here until the crawling is finished
